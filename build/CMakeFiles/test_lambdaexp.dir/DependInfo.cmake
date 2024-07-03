96edd4003a44f64f9f3599dbaf8e7f0764392ada
vboxuser <vboxuser@UbuntuVM.myguest.virtualbox.org>
2024-07-03 13:51:46 +0530

    Lambda Expressions

---
+++ build/CMakeFiles/test_lambdaexp.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/vboxuser/Workspace/CppPractice/CppPractice/01_Lambda_exp.cc" "CMakeFiles/test_lambdaexp.dir/01_Lambda_exp.cc.o" "gcc" "CMakeFiles/test_lambdaexp.dir/01_Lambda_exp.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/test_lambdaexp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_lambdaexp.dir/01_Lambda_exp.cc.o"
+  "CMakeFiles/test_lambdaexp.dir/01_Lambda_exp.cc.o.d"
+  "test_lambdaexp"
+  "test_lambdaexp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_lambdaexp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
