042d0644e0096d42ee8f26018e5273c8678a7997
SairamanG <84399064+sairamang@users.noreply.github.com>
2024-07-26 00:25:44 +0000

    Virtual Inheritance code & error png

---
+++ 03_VirtualInheritance.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+struct A
+{
+  void fun()
+{
+  std::cout<<"A"<<std::endl;
+}
+};
+struct B : public virtual A 
+{
+
+};
+struct C: public virtual  A 
+{
+
+};
+struct D:public B , public C
+{
+  void fun1()
+{
+  fun();
+}
+};
+int main()
+{
+  D d_obj;
+  d_obj.fun();
+  return 0;
+}
