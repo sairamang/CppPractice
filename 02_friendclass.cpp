2b58956f7f9eb27957205f64d036fec90a9345cf
SairamanG <84399064+sairamang@users.noreply.github.com>
2024-07-26 00:12:17 +0000

    Practice Friend class

---
+++ 02_friendclass.cpp
@@ -0,0 +1,21 @@
+#include "02_friendclass.hpp"
+
+std::ostream& operator<<(std::ostream& os , Animal& animal)
+{
+    std::cout<<"Animal height"<<animal.m_height<<std::endl;
+    return os;
+}
+
+void AnimalPrinter::print(Animal& animal)
+{
+  std::cout<<"Animal weight"<<animal.m_weight<<std::endl;
+}
+
+// #include "friendclass.hpp"
+// int main()
+// {
+//   Animal animal_obj(10,20);
+//   AnimalPrinter aptr;
+//   aptr.print(animal_obj);
+//   std::cout<<animal_obj;
+// }
---
+++ 02_friendclass.hpp
@@ -0,0 +1,22 @@
+#include<iostream>
+class AnimalPrinter;
+class Animal
+{
+  private:
+  int m_height;
+  int m_weight;
+  friend class AnimalPrinter;
+  
+  public:
+  
+  Animal(int height , int weight):m_height(height),m_weight(weight)
+{
+  
+}
+friend std::ostream& operator<<(std::ostream& os ,Animal& animal);
+};
+class AnimalPrinter
+{
+public:
+  void print(Animal& animal);
+};
