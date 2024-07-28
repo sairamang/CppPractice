093732411172cff31eda254c007eb88c3c111509
SairamanG <84399064+sairamang@users.noreply.github.com>
2024-07-28 07:24:28 +0000

    Const member functions , const variables

---
+++ 05_constMemberFunctions.cpp
@@ -0,0 +1,17 @@
+// #include <iostream>
+// using namespace std;
+// class MyClass {
+// private:
+//   int myVar;
+
+// public:
+//   void setInt(int a)  { myVar = a; }
+//   int getVar() const { return myVar; }
+// };
+
+// int main() {
+//   MyClass a;
+//   a.setInt(10);
+//   cout << "Var : " << a.getVar() << endl;
+//   return 0;
+// }
---
+++ 06_constVariablesAndPointer.cpp
@@ -0,0 +1,22 @@
+// int main()
+// {
+
+//   const int a = 10;//const variable to 10
+//   const int b = 20;
+//   const int* const pC = &a; //const pointer to const
+
+//   int c =1 ;
+//   int d = 2;
+//   const int& ref = a;// const reference to a
+//   //pointer to a constant
+//   const int* ptr = &a;//const pointer to a  //Cannot change A from ptr
+//   //(*ptr)++; //error
+
+//   //constant pointer
+//   int* const ptr2 =&c;
+//   //ptr2=&d;//error //cannot change ptr2 to point to d
+//   // (*pC)++ //error cannot change
+//   // pC =&b;// error cannot change the value it points
+
+//   return 0;
+// }
